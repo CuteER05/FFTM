3885c36540f8b4504037fd3082b616e35fad85b1
CuteER05 <esterruth.tevesoredo@gmail.com>
2024-03-29 23:15:48 +0800

    changed constant

--- lib/dbHelper/constant.dart
+++ lib/dbHelper/constant.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: constant_identifier_names
+
 const MONGO_CONN_URL =
     "mongodb+srv://Admin:admin@ftm.tk6c2cu.mongodb.net/FTM?retryWrites=true&w=majority&appName=FTM";
 const USER_COLLECTION = "feelthemusic";
